f5eed68ecaf3d36a5527b63b46696c16f94cb1f7
Samuel <samawyck@gmail.com>
2025-12-10 13:55:05 -0600

    Create algorithm manager class

---
+++ src/algorithmManger.js
@@ -0,0 +1,84 @@
+const BreadthFirstSearch = require("./searchAlgorithms/breadthFirstSearch.js");
+const DepthFirstSearch = require("./searchAlgorithms/depthFirstSearch.js");
+
+
+
+class AlgorithmManger {
+    constructor(width, height) {
+        this.wallSymbol = "W";
+        this.goalSymbol = "G";
+        this.startSymbol = "S";
+        this.emptySymbol = "O";
+
+        this.bfs = new BreadthFirstSearch(this.wallSymbol, this.goalSymbol);
+        this.dfs = new DepthFirstSearch(this.wallSymbol, this.goalSymbol);
+
+        this.board = this.#createBoard(width, height);
+
+        this.startRow = null;
+        this.startCol = null;
+    };
+
+    #createBoard(width, height) {
+        const board = [];
+        for (let row = 0; row < height; row += 1) {
+            const row = [];
+            for (let col = 0; col < width; col += 1) {
+                row.push(this.emptySymbol);
+            }
+            board.push(row);
+        }
+        return board;
+    };
+
+    breadthFirstSearch() {
+        if (!this.startRow || !this.startCol) {
+            return null;
+        }
+
+        const bfsPath = this.bfs.breadthFirstPath(this.grid, this.startRow, this.startCol);
+        const shortestPath = this.dfs.shortestPath(this.grid, this.startRow, this.startCol);
+        return [bfsPath, shortestPath];
+    };
+
+    depthFirstSearch() {
+        if (!this.startRow || !this.startCol) {
+            return null;
+        }
+
+        const dfsPath = this.dfs.dfsPath(this.grid, this.startRow, this.startCol);
+        const shortestPath = this.dfs.shortestPath(this.grid, this.startRow, this.startCol);
+        return [dfsPath, shortestPath];
+    };
+
+    resetBoard() {
+        for (let row = 0; row < this.board.length; row += 1) {
+            for (let col = 0; col < this.board[row].length; col += 1) {
+                this.board[row][col] = this.emptySymbol;
+            }
+        }
+    };
+
+    setBoard(board) {
+        for (let row = 0; row < this.board.length; row += 1) {
+            for (let col = 0; col < this.board[row].length; col += 1) {
+                const newSymbol = board[row][col];
+                if (newSymbol === this.startSymbol) {
+                    this.startRow = row;
+                    this.startCol = col;
+                    this.board[row][col] = this.emptySymbol;
+                } else {
+                    this.board[row][col] = newSymbol;
+                }
+            }
+        }
+    };
+
+    setCell(row, col, symbol) {
+        this.board[row][col] = symbol;
+    };
+};
+
+
+
+module.exports = AlgorithmManger;
