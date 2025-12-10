e336fb491f9baee620d7e653269fe3ce5f83134f
Samuel <samawyck@gmail.com>
2025-12-10 11:37:14 -0600

    Create depthFirstSearch class

---
+++ src/searchAlgorithms/depthFirstSearch.js
@@ -0,0 +1,107 @@
+class DepthFirstSearch {
+    constructor() {
+        this.goalSymbol = "G";
+        this.wallSymbol = "W";
+        this.emptySymbol = "O";
+    };
+
+    shortestPath(grid, startRow, startCol) {
+        const visited = new Set();
+        const memo = {};
+        const shortestPath = this.#findShortestPath(grid, startRow, startCol, visited, memo);
+        shortestPath.reverse();
+        return shortestPath;
+    };
+
+    #findShortestPath(grid, row, col, visited, memo) {
+        const rowValid = 0 <= row && row < grid.length;
+        const colValid = 0 <= col && col < grid[0].length;
+        if (!rowValid || !colValid) {
+            return [];
+        }     
+        const key = JSON.stringify([row, col]);
+        if (visited.has(key)) {
+            return [];
+        }
+        if (key in memo) {
+            return memo[key];
+        }
+        const symbol = grid[row][col];
+        if (symbol === this.wallSymbol) {
+            return [];
+        }
+        const position = [row, col];
+        if (symbol === this.goalSymbol) {
+            return [position];
+        }
+
+        const neighbors = [
+            [row - 1, col], [row + 1, col],
+            [row, col - 1], [row, col + 1]
+        ];
+
+        visited.add(key);
+        let minPath = [];
+        for (let neighbor of neighbors) {
+            const [nRow, nCol] = neighbor;
+            const subPath = this.#findShortestPath(grid, nRow, nCol, visited, memo);
+            if (subPath.length === 0) {
+                continue
+            }
+            minPath = (minPath.length === 0 || subPath.length < minPath.length) ? subPath : minPath;
+        }
+        visited.delete(key);
+
+        if (minPath.length > 0) {
+            minPath.push(position);
+        }
+        memo[key] = minPath.slice();
+        return minPath;
+    };
+
+    dfsPath(grid, startRow, startCol) {
+        const path = [];
+        const visited = new Set();
+        this.#dsfPath(grid, startRow, startCol, visited, path);
+        return path;
+    };
+
+    #dsfPath(grid, row, col, visited, path) {
+        const rowValid = 0 <= row && row < grid.length;
+        const colValid = 0 <= col && col < grid[0].length;
+        if (!rowValid || !colValid) {
+            return false;
+        }
+        const key = JSON.stringify([row, col]);
+        if (visited.has(key)) {
+            return false;
+        }
+        const symbol = grid[row][col];
+        if (symbol === this.wallSymbol) {
+            return false;
+        }
+        path.push([row, col]);
+        if (symbol === this.goalSymbol) {
+            return true;
+        }
+
+        visited.add(key);
+        const neighbors = [
+            [row - 1, col], [row + 1, col],
+            [row, col - 1], [row, col + 1]
+        ];
+
+        for (let neighbor of neighbors) {
+            const [nRow, nCol] = neighbor;
+            if (this.#dsfPath(grid, nRow, nCol, visited, path)) {
+                return true
+            }
+        }
+
+        return false;
+    };
+};
+
+
+
+module.exports = DepthFirstSearch;
